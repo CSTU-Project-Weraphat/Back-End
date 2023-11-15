b1c9a08403844537d256c5634fbd64329cf4989c
park <zparkasv2@gmail.com>
2023-11-15 18:25:28 +0700

    Edit API GET ScholarshipType

--- controller/scholarshipType/handler.js
+++ controller/scholarshipType/handler.js
@@ -3,10 +3,14 @@ const executeQuery = require("../../utils/pool_connections");
 const handlerScholarshipType = (req, res) => {
     const query = `SELECT 
         scholarship_type_id, 
-        scholarship_type_name, 
-        create_date, 
+        scholarship_type_name,
+        is_active, 
+        create_date 
     FROM 
-        scholarship_type`;
+        scholarship_type
+    WHERE
+        is_active = 'Y'`;
+        
     executeQuery(query, (data)=>{
         res.send({result:data.rows})
     });
